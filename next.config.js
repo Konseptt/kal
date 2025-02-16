cd04d4d771461005ccd9ec5b43112df380593cf1
konsept <sharmaranjan816@gmail.com>
2025-02-16 20:02:02 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
---
+++ out/_next/static/JxMnLCan4leyGPiUrhTrg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-96758481f1d32622.js","static/chunks/106-6504b81ba52ea9a6.js","static/chunks/pages/index-e022ec51805e2f97.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
